d66f7ded81cd63c536337ef1ced38f710b5d60a8
Leonardotorre18 <Leonardotorre14@gmail.com>
2023-10-10 18:50:44 -0400

    Installed Morgan

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import routes from "../routes";
 import cors from 'cors';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ mongoose.connect(process.env.DATABASE_URL || '')
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/', routes);
 
