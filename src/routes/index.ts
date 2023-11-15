5defa3604094d4df191b2373a96e716a15cac486
leonardotorre18 <leonardotorre14@gmail.com>
2023-11-15 13:30:27 -0400

    New Cinema Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,16 @@
-import express, { Express } from "express";
-import musicRoute from './music';
-import moviesRoute from './movies';
-// import formRoute from './form';
-import socialRoute from './social'
+import express from 'express'
+import musicRoute from './music'
+// import moviesRoute from './movies'
+// import formRoute from './form'
+import cinemaRouter from './cinema'
+// import socialRoute from './social'
 
-const server: Express = express();
+const server: express.Express = express()
 
-server.use('/music', musicRoute);
-server.use('/movies', moviesRoute);
+server.use('/music', musicRoute)
+// server.use('/movies', moviesRoute)
+server.use('/cinema', cinemaRouter)
 // server.use('/form', formRoute);
+// server.use('/social', socialRoute)
 
-
-server.use('/social', socialRoute)
-
-
-
-export default server;
+export default server
